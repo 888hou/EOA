623e88acce805160f1474efcbc46621d20832c14
ExtreLife <xxf1581988738@gmail.com>
2021-08-15 19:44:29 +0800

    feat: 添加获取验证码接口

---
+++ EOA.Api/Controllers/CaptchaController.cs
@@ -0,0 +1,32 @@
+﻿using EOA.Helper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EOA.Api.Controllers
+{
+    /// <summary>
+    /// 验证码
+    /// </summary>
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CaptchaController : ControllerBase
+    {
+        /// <summary>
+        /// 获取随机验证码
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<FileContentResult> GetCaptcha()
+        {
+            var code = await CaptchaHelper.GenerateRandomCaptchaAsync();
+
+            var result = await CaptchaHelper.GenerateCaptchaImageAsync(code);
+
+            return File(result.CaptchaMemoryStream.ToArray(), "image/png");
+        }
+    }
+}
--- EOA.Helper/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace EOA.Helper
-{
-    public class Class1
-    {
-    }
-}
